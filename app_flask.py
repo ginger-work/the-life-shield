0b2d4eb90b0ebece858a398e3dd12335bb317142
Ginger Development Team <ginger@thelifeshield.local>
2026-04-14 09:59:00 -0400

    FIX: Explicit CORS headers in after_request handler

--- app_flask.py
+++ app_flask.py
@@ -10,6 +10,15 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 users = {}
 
+@app.after_request
+def after_request(response):
+    """Explicitly add CORS headers to every response."""
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Max-Age'] = '3600'
+    return response
+
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({
